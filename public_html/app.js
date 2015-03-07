34bd34fe6c4d31121446919a3020407db588cf02
kuidaoring <kuijob@gmail.com>
2015-03-08 01:26:27 +0900

    fix indent.

--- public_html/app.js
+++ public_html/app.js
@@ -1,87 +1,69 @@
 $(function() {
-    
 
-  var NAME = "mbed HRM1017";
-  var PIN = 5;
+var NAME = "mbed HRM1017";
+var PIN = 5;
+var UP = k.LOW
+var DOWN = k.HIGH;
+var USERS = {
+        ysasaki: [
+            "うんこ",
+            "Perfume",
+            "ONE OK ROCK",
+        ],
+        kinadu: [],
+        yeshi: [
+            "フォクすけ",
+        ],
+        yuujsato: [
+        ]
+};
+var user = USERS.yuujsato;
 
-  var UP = k.LOW
-  var DOWN = k.HIGH;
-  
-  var USERS = {
-    ysasaki: [
-      "うんこ",
-      "Perfume",
-      "ONE OK ROCK",
-    ],
-     kinadu: [
-    ],
-    yeshi: [
-      "フォクすけ",
-    ],
-     yuujsato: [
-    ]
-  };
-  var user = USERS.yuujsato;
-
-
-  k.find();
-  k.ready(function() {
+k.ready(function() {
     k.digitalWrite(PIN, DOWN);
-  });
+});
 
-  /*
-  $("body").on("touchstart", function() {
-    return false;
-  });
-  */
-  
-  /*
-  {
-    key: "1",
-    state: "UP" // or "DOWN"
-  }
-  */
-  
-  var milkcocoa_app_id = 'io-ji6yr5qe9';    
-  var milkcocoa = new MilkCocoa("https://" + milkcocoa_app_id + ".mlkcca.com/");
-  var ds = milkcocoa.dataStore("message");  
-  
-  //まるっと削除
-  ds.query().done(function(data) {
+k.find();
+var milkcocoa_app_id = 'io-ji6yr5qe9';
+var milkcocoa = new MilkCocoa("https://" + milkcocoa_app_id + ".mlkcca.com/");
+var ds = milkcocoa.dataStore("message");
+
+//まるっと削除
+ds.query().done(function(data) {
     data.forEach(function(value) {
-      ds.remove(value.id);
+        ds.remove(value.id);
     });
-  });
-  
-  
-  //リアルタイムに変更を監視
-  ds.on("push", function(data) {
+});
+
+
+//リアルタイムに変更を監視
+ds.on("push", function(data) {
     if (data.value.state && data.value.state === "UP"){
-      if (user.length === 0 || $.inArray(data.value.key, user)) {
-        k.digitalWrite(PIN, UP);
-      }
-      console.log(data.value.key);
+        if (user.length === 0 || $.inArray(data.value.key, user)) {
+            k.digitalWrite(PIN, UP);
+        }
+        console.log(data.value.key);
     } else {
-      if (user.length === 0 || $.inArray(data.value.key, user)) {
-        k.digitalWrite(PIN, DOWN);
-      }
-      console.log(data.value.key);
+        if (user.length === 0 || $.inArray(data.value.key, user)) {
+            k.digitalWrite(PIN, DOWN);
+        }
+        console.log(data.value.key);
     }
     $('#debug').append($('<div>').text(data.value.state));
-  });
-      
-  $('.syumiList li').on("touchstart", function(e) {
+});
+
+$('.syumiList li').on("touchstart", function(e) {
     ds.push({
-      state: "UP", 
-      key: $(e.target).text(),
+        state: "UP",
+        key: $(e.target).text(),
     });
-  });
-  
-  $(".syumiList li").on("touchend", function(e) {
+});
+
+$(".syumiList li").on("touchend", function(e) {
     ds.push({
-      state: "DOWN", 
-      key: $(e.target).text(),
+        state: "DOWN",
+        key: $(e.target).text(),
     });
-  });
-    
+});
+
 });
