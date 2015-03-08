ed1091bd6d66953ec2c7be3ac4a4081774aa1821
Yeshi.jp <yoshikema@gmail.com>
2015-03-08 11:15:35 +0900

    人リストつくった

--- public_html/personList.php
+++ public_html/personList.php
@@ -1,15 +1,75 @@
-<ul id="personList" class="disable">
+<div id="personList" class="disable">
+
+<ul>
+
 <li class="person">
-<p class="image"><img src=""></p>
-<p class="name"></p>
+<div class="hito">
+<p class="image"><img src="image/nadou.png" width="40"></p>
+<p class="name">などう</p>
+</div>
 <ul class="kyoumi">
-<li></li>
+<li>♥ 電子工作</li>
+<li>♥ JavaScript</li>
+<li>♥ 魔法陣</li>
+<li>♥ 竹田城址</li>
 </ul>
 <ul class="account">
-<li>Twitter</li>
-<li>Facebook</li>
-<li>Github</li>
+<li class="tw">Twitter</li>
+<li class="fb">Facebook</li>
+<li class="gh">Github</li>
 </ul>
 </li>
+
+<li class="person">
+<div class="hito">
+<p class="image"><img src="image/yeshi.png" width="40"></p>
+<p class="name">いぇし</p>
+</div>
+<ul class="kyoumi">
+<li>♥ フォクすけ</li>
+<li>♥ Firefox</li>
+<li>♥ 乗り鉄</li>
+<li>♥ ビール</li>
+</ul>
+<ul class="account">
+<li class="tw">Twitter</li>
+<li class="gh">Github</li>
+</ul>
+</li>
+
+<li class="person">
+<div class="hito">
+<p class="image"><img src="image/shika.png" width="40"></p>
+<p class="name">しか</p>
+</div>
+<ul class="kyoumi">
+<li>♥ ビール</li>
+<li>♥ 野球</li>
+<li>♥ レガシーコード</li>
+</ul>
+<ul class="account">
+<li class="fb">Facebook</li>
+<li class="gh">Github</li>
+</ul>
+</li>
+
+<li class="person">
+<div class="hito">
+<p class="image"><img src="https://pbs.twimg.com/profile_images/551009294741565440/OaCR4rwA_reasonably_small.jpeg" width="40"></p>
+<p class="name">ゆんこ</p>
+</div>
+<ul class="kyoumi">
+<li>♥ うんこ</li>
+<li>♥ Perfume</li>
+<li>♥ ラーメン</li>
+</ul>
+<ul class="account">
+<li class="tw">Twitter</li>
+<li class="fb">Facebook</li>
+</ul>
+</li>
+
+
 </ul>
 
+</div><!-- /personList -->
